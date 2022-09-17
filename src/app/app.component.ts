aa34cfd4af77c73dc5db27a4a1adc01997b615ea
praveshstark <pravesh.yadav.virat@gmail.com>
2022-09-17 19:39:07 +0530

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Blog Application';
+  title = 'Blog Application: Client';
 }
